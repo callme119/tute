378c536ad4194b228a215445040a504c2880fd6a
xuao <295184686@qq.com>
2015-07-12 21:10:28 +0800

    Menu部门代码编写

--- Application/Menu/Controller/IndexController.class.php
+++ Application/Menu/Controller/IndexController.class.php
@@ -16,32 +16,17 @@ class IndexController extends AdminController
     
     public function indexAction()
     {
-        $menuModel = new MenuModel();
-        $data = $menuModel->getMenuTree();
-        var_dump($data);
+        //$menuModel = new MenuModel();
+        //$data = $menuModel->getMenuTree();
+        //var_dump($data);
         //从数据库中取出菜单信息，加在这，与下边的url的id对应
-        $url = array(
-            'addRoot'=>U('addRootMenu'),
-            'addSon'=>U('addSonMenu?id=$id'),
-            'editMenu'=>U('editMenu'));
+        $url = U('addOrEdit');
         $this->assign('url',$url);
-        //$this->assign('YZBODY',$this->fetch());
-       // $this->display(YZTemplate);
-    }
-    public function addRootMenuAction(){
-        $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
-    }
-    public function addSonMenuAction(){
-        $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
-    }
-    public function editMenuAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);
     }
     /**
-     * 保存子菜单函数
+     * 保存函数
      * 与数据库连接，将数据保存到菜单表中
      * 无返回值，保存成功后调用成功界面，并返回index页面
      * 2015年7月9日20:17:15
@@ -68,6 +53,10 @@ class IndexController extends AdminController
     public function addOkAction(){
         $url = U('index');
         $this->success('保存成功',$url);
+    }
+    public function addOrEditAction(){
+        $this->assign('YZBODY',$this->fetch());
+        $this->display(YZTemplate);
     }
 }
 
