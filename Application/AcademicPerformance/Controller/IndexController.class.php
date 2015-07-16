29f6a73e0be12b5252e05712b44081ca6e6fdb8c
githubXLJ <164408119@qq.com>
2015-07-16 15:44:16 +0800

    添加教学建设，学科业绩，完成服务育人界面

--- Application/AcademicPerformance/Controller/IndexController.class.php
+++ Application/AcademicPerformance/Controller/IndexController.class.php
@@ -1,17 +1,17 @@
 <?php
-/*服务育人模块
+/*学科业绩模块
  * author:xulinjie
  * email:164408119@qq.com
  * create date:2015.07.16
  */
-namespace ServiceEducation\Controller;
+namespace AcademicPerformance\Controller;
 use Admin\Controller\AdminController;
 class IndexController extends AdminController {
     public function indexAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
     }
-    //服务育人的添加
+    //学科业绩的添加
     public function addAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
--- Application/TeachingConstruction/Controller/IndexController.class.php
+++ Application/TeachingConstruction/Controller/IndexController.class.php
@@ -1,17 +1,17 @@
 <?php
-/*服务育人模块
+/*教学建设模块
  * author:xulinjie
  * email:164408119@qq.com
  * create date:2015.07.16
  */
-namespace ServiceEducation\Controller;
+namespace TeachingConstruction\Controller;
 use Admin\Controller\AdminController;
 class IndexController extends AdminController {
     public function indexAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
     }
-    //服务育人的添加
+    //教学建设的添加
     public function addAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
