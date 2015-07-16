5a7fb8f6cc4b7e55cd0fcef4138d006d21d22e6e
githubXLJ <164408119@qq.com>
2015-07-16 18:53:14 +0800

    学科业绩的3个添加界面

--- Application/AcademicPerformance/Controller/IndexController.class.php
+++ Application/AcademicPerformance/Controller/IndexController.class.php
@@ -11,8 +11,18 @@ class IndexController extends AdminController {
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
     }
-    //学科业绩的添加
-    public function addAction(){
+    //研究生教学的添加
+    public function graduateAddAction(){
+        $this->assign('YZBODY',$this->fetch());
+        $this->display(YZTemplate);  
+    }
+    //学科与科研平台的添加
+    public function platformAddAction(){
+        $this->assign('YZBODY',$this->fetch());
+        $this->display(YZTemplate);  
+    }
+    //科研奖励的添加
+    public function ScientificAwardAddAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
     }
