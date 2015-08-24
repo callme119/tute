5ab4e8e20f34c80477fcd10a307196441df76ae1
myskies <3792535@qq.com>
2015-08-24 21:46:34 +0800

    更改数据库配置为服务器
    本地有需求请自行修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -5,10 +5,11 @@ return array(
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => 'localhost', // 服务器地址www.callme119.com
+    'DB_HOST'   => 'www.callme119.com', 
     'DB_NAME'   => 'performancems', // 数据库名performancems
-    'DB_USER'   => 'root', // 用户名performancems
-    'DB_PWD'    => '',  // 密码b2408cac49ed15d67c390dd08a8b0158
-    'DB_PORT'   => '3366', // 端口3306
+    'DB_USER'   => 'performancems', // 用户名performancems
+    'DB_PWD'    => 'b2408cac49ed15d67c390dd08a8b0158',  // 密码b2408cac49ed15d67c390dd08a8b0158
+    'DB_PORT'   => '3306', // 端口3306
     'DB_PREFIX' => 'yunzhi_', // 数据库表前缀
     'UPLOAD_ROOT_PATH' => '/Uploads',//附件上传根路径
     'SESSION_AUTO_START' => true, //开启session
