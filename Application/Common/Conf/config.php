998a09dca7837158c7f70afc4fd0442cb7b2681b
weijingyun <1028193951@qq.com>
2015-08-24 15:14:47 +0800

    魏静云

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -4,10 +4,10 @@ return array(
     'ACTION_SUFFIX'         => 'Action',//设置ACTION前缀为action
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'performance', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '',  // 密码
+    'DB_HOST'   => 'www.callme119.com', // 服务器地址
+    'DB_NAME'   => 'performancems', // 数据库名
+    'DB_USER'   => 'performancems', // 用户名
+    'DB_PWD'    => 'b2408cac49ed15d67c390dd08a8b0158',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'yunzhi_', // 数据库表前缀
     'UPLOAD_ROOT_PATH' => '/Uploads',//附件上传根路径
