2b22eb5fce0f22368f9741fbc81273939b6395ef
xuao <295184686@qq.com>
2015-08-25 19:16:56 +0800

    Revert "Revert "角色管理列表显示""
    This reverts commit 15c0887bf7fcf2c7e84fb2fb8bd83b7a653c9b1e.

--- Application/Role/Controller/IndexController.class.php
+++ Application/Role/Controller/IndexController.class.php
@@ -1,12 +1,6 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Description of IndexController
  *
  * @author xuao
  */
@@ -15,49 +9,56 @@ use Admin\Controller\AdminController;
 use Role\Model\RoleModel;
 class IndexController extends AdminController
 {
+    //初始化方法
     public function indexAction()
     {
+        //获取page信息
         $page = I('get.id',1);
-        $url=array(
-            "addRole"=>U('addRole'),
-            "editRole"=>U('editRole'),
-            "deleteRole"=>U('deleteRole'),
-            "people"=>U('people'),
-            );
+        //获取角色列表
         $roleModel = new RoleModel();
         $roleList = $roleModel->getRoleList($page);
-        $this->assign('roleLIst',$roleLIst);
-        $this->assign('url',$url);
+        $roleList = $this->_addurl($roleList,"_url");
+        //传值
+        $this->assign('roleList',$roleList);
         $this->assign('YZBODY',$this->fetch());
+        //前台显示
         $this->display(YZTemplate);
     }
+    //添加角色
     public function addRoleAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);
     }
+    //显示角色人员信息
     public function peopleAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);
     }
+    //删除角色
     public function deleteRoleAction(){
 
     }
+    //编辑角色
     public function editRoleAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);
     }
+    //保存
      public function saveOkAction(){
         $url = U('index');
         $this->success('保存成功',$url);
     }
+    //添加
     public function addOkAction(){
         $url = U('index');
         $this->success('保存成功',$url);
     }
+    //移除用户
     public function movePeopleOkAction(){
         $url = U('people');
         $this->success('保存成功',$url);
     }
+    //添加用户
     public function addPeopleOkAction(){
         $url = U('people');
         $this->success('保存成功',$url);
@@ -65,5 +66,22 @@ class IndexController extends AdminController
     public function addPeopleAction(){
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);
+    }
+    /**
+     * 添加url信息
+     * @param  [type]
+     * @param  [type]
+     * @return [type]
+     */
+    private function _addurl($array,$string){
+        $data = $array;
+        foreach ($data as $key => $value) {
+            $data[$key][$string] = array(
+                'edit'=>U('editRole?id='.$value['id']),
+                'delete'=>U('deleteRole?id='.$value['id']),
+                'people'=>U('people?id='.$value['id']),
+                );
+        }
+        return $data;
     }
 }
