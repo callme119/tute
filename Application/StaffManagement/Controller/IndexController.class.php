bcb43666e9df2f3a72b1244b94ef505cfd974a64
xuao <295184686@qq.com>
2015-09-01 11:10:25 +0800

    教工编辑和教工删除
    遗留问题：添加教工部分前台界面问题，
    添加教工模块，岗位，角色，部门 从数据库获取
    重置密码功能
    状态切换问题

--- Application/StaffManagement/Controller/IndexController.class.php
+++ Application/StaffManagement/Controller/IndexController.class.php
@@ -14,8 +14,8 @@ class IndexController extends AdminController {
         //获取教工列表
         $staffModel = new StaffManagementModel;
         $staffList = $staffModel -> getStaffList();
-        $staffList = $this -> _addurl($staffList);
-
+        $staffList = $this -> _addurl($staffList,'_url');
+        //var_dump($staffList);
         $this->assign('staffList',$staffList);
         $this->assign('css',$this->fetch("addCss"));
         $this->assign('YZBODY',$this->fetch());
@@ -27,28 +27,53 @@ class IndexController extends AdminController {
         //获取岗位列表信息(考虑是否加到初始化方法中，因为编辑也需要)
         //获取角色列表信息(考虑是否加到初始化方法中，因为编辑也需要)
         //传值，前台进行处理
+        $url = U('save');
+        $this->assign('url',$url);
         $this->assign('css',$this->fetch("addCss"));
         $this->assign('YZBODY',$this->fetch());
         $this->display(YZTemplate);  
     }
     //教工管理编辑
     public function editAction(){
+        $id = I('get.id');
+        //获取当前教工的信息
+        $staffModel = new StaffManagementModel;
+        $staffInfo = $staffModel -> getStaffById($id);
+
+        //传值
+        $url = U('update?id='.$id);
+        $this->assign('url',$url);
+        $this ->assign('staffInfo',$staffInfo);
         $this->assign('YZBODY',$this->fetch('add'));
         $this->display(YZTemplate);  
+    }
+    //删除教工
+    public function deleteAction(){
+        $id = I('get.id');
+        $staffModel = new StaffManagementModel;
+        $state = $staffModel -> deleteStaff($id);
+        if($state){
+            $this->success('删除成功', U('index'));
+        }
     }
     //添加教工完成
     public function saveAction(){
         $staffModel = new StaffManagementModel;
         $state = $staffModel -> addStaff();
+
         if($state){
             $this->success('新增成功', 'index');
         }
-
     }
     //编辑教工完成
     public function updateAction(){
-
+        $staffModel = new StaffManagementModel;
+        $state = $staffModel -> updateStaff();
+        if($state){
+            $this->success('修改成功', U('index'));
+        }
     }
+
     /**
      * 添加url信息
      * @param  [type] 要添加的数组
--- Application/StaffManagement/Model/StaffManagementModel.class.php
+++ Application/StaffManagement/Model/StaffManagementModel.class.php
@@ -9,8 +9,15 @@
 namespace StaffManagement\Model;
 use Think\Model;
 class StaffManagementModel extends Model{
+	//获取教工列表
 	public function getStaffList(){
 		$data = $this -> select();
+		return $data;
+	}
+	//获取固定id的教工的信息
+	public function getStaffById($id){
+		$map['id'] = $id;
+		$data = $this -> where($map) ->find();
 		return $data;
 	}
 	//添加教工
@@ -18,5 +25,17 @@ class StaffManagementModel extends Model{
 		$data = I('post.');
 		$this->add($data);
 		return true;
+	}
+	//编辑教工
+	public function updateStaff(){
+		$data = I('post.');
+		$data[id] = I('get.id');
+		$this -> save($data);
+		return true;
+	}
+	public function deleteStaff(){
+		$data[id] = I('get.id');
+		$state = $this -> where($data) ->delete();
+		return $state;
 	}
 }
