22825ec15857d4e82986c5b03f1da9e4455f0080
myskies <3792535@qq.com>
2015-09-03 22:53:37 +0800

    将原来教工管理引用

--- Application/User/Controller/IndexController.class.php
+++ Application/User/Controller/IndexController.class.php
@@ -1,41 +1,93 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Description of IndexController
- *
- * @author xuao
+/*教工管理模块
+ * author:weijingyun
+ * email:1028193951@qq.com
+ * create date:2015.07.16
  */
 namespace User\Controller;
 use Admin\Controller\AdminController;
-class IndexController extends AdminController
-{
-    public function indexAction()
-    {
+use User\Model\UserModel;
+class IndexController extends AdminController {
+
+    //教工列表显示
+    public function indexAction(){
+        //获取教工列表
+        $staffModel = new UserModel();
+        $staffList = $staffModel -> getStaffList();
+        $staffList = $this -> _addurl($staffList,'_url');
+        //var_dump($staffList);
+        $this->assign('staffList',$staffList);
+        $this->assign('css',$this->fetch("addCss"));
         $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
+        $this->display(YZTemplate); 
     }
-    public function addUserAction()
-    {
+    //教工管理添加教工
+    public function addAction(){
+        //获取部门列表信息(考虑是否加到初始化方法中，因为编辑也需要)
+        //获取岗位列表信息(考虑是否加到初始化方法中，因为编辑也需要)
+        //获取角色列表信息(考虑是否加到初始化方法中，因为编辑也需要)
+        //传值，前台进行处理
+        $url = U('save');
+        $this->assign('url',$url);
+        $this->assign('css',$this->fetch("addCss"));
         $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
+        $this->display(YZTemplate);  
     }
-    public function editUserAction()
-    {
-        $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
+    //教工管理编辑
+    public function editAction(){
+        $id = I('get.id');
+        //获取当前教工的信息
+        $staffModel = new UserModel();
+        $staffInfo = $staffModel -> getStaffById($id);
+
+        //传值
+        $url = U('update?id='.$id);
+        $this->assign('url',$url);
+        $this ->assign('staffInfo',$staffInfo);
+        $this->assign('YZBODY',$this->fetch('add'));
+        $this->display(YZTemplate);  
     }
-    public function reviseAction()
-    {
-        $this->assign('YZBODY',$this->fetch());
-        $this->display(YZTemplate);
+    //删除教工
+    public function deleteAction(){
+        $id = I('get.id');
+        $staffModel = new UserModel();
+        $state = $staffModel -> deleteStaff($id);
+        if($state){
+            $this->success('删除成功', U('index'));
+        }
     }
-    public function successAction(){
-        $url = U('index');
-        $this->success('保存成功',$url);
+    //添加教工完成
+    public function saveAction(){
+        $staffModel = new UserModel();
+        $state = $staffModel -> addStaff();
+
+        if($state){
+            $this->success('新增成功', 'index');
+        }
+    }
+    //编辑教工完成
+    public function updateAction(){
+        $staffModel = new UserModel();
+        $state = $staffModel -> updateStaff();
+        if($state){
+            $this->success('修改成功', U('index'));
+        }
+    }
+
+    /**
+     * 添加url信息
+     * @param  [type] 要添加的数组
+     * @param  [type] 要填写的下标名
+     * @return [type] 拼接好url信息的数组
+     */
+    private function _addurl($array,$string){
+        $data = $array;
+        foreach ($data as $key => $value) {
+            $data[$key][$string] = array(
+                'edit'=>U('edit?id='.$value['id']),
+                'delete'=>U('delete?id='.$value['id']),
+                );
+        }
+        return $data;
     }
 }
