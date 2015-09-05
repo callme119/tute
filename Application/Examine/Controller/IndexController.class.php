49d689237b6463ffa1fb90eda7bc18cf55ac41ef
myskies <3792535@qq.com>
2015-09-05 17:35:16 +0800

    增加getExamineUsersAction用于显示下一审核人

--- Application/Examine/Controller/IndexController.class.php
+++ Application/Examine/Controller/IndexController.class.php
@@ -41,6 +41,39 @@ class IndexController extends AdminController{
         $name = I('post.name');
         $model->save($id,$name);
     }
-    
+    /**
+     * 通过GET过来的流程ID值，查找当前用户，当前流程下的下一环节审核人员
+     * @param [string] $get.id 传入的用户选择的流程ID值
+     * @return [二维数组] 下一环节审核人员ID及姓名
+     * todo:重写get_user_id()。原因，此时如果判断出用户未登陆，不应该直接跳出，而应该返回FALSE。
+     */
+    public function getExamineUsersAction()
+    {   
+        //初始化数据
+        $examineId = I("get.id");
+        $state = 'success';
+
+        //判断空值 
+        if( empty($id) )
+        {
+            $state = 'error';
+            $data['message'] = '未传入正确的ID值，或传入的ID值有误';
+        }
+
+        //程序开始执行
+        else
+        {
+            $userId = get_user_id();
+
+            //取用户在当前审核流程下适用的岗位集合（一组数组）
+            //取在此岗位下，用户的部门及上级部门（三维数组）。
+            //
+            $data = array();
+        }
+
+        //返回数据
+        $return = array('state' => $state,'data'=> $data);
+        $this->ajaxReturn($return);
+    }
 }
     
