b1d8f4d92dae5f85bc03be3857ab75818caffcb2
myskies <3792535@qq.com>
2015-09-06 15:20:35 +0800

    完善云智标签

--- ThinkPHP/Library/Think/Template/TagLib/Yunzhi.class.php
+++ ThinkPHP/Library/Think/Template/TagLib/Yunzhi.class.php
@@ -19,8 +19,8 @@ class Yunzhi extends TagLib {
     	//字符串接拼。会直接传到前面的页面，为避免冲突，变量前加yunzhi_
         $parseStr = '<?php ';
         $parseStr .= '$yunzhi_UserM = new User\Model\UserModel();';
-        $parseStr .= '$' . $name . '= $yunzhi_UserM->getUserById(';
-        $parseStr .= "'$id'"; //双引号进行变量替换
+        $parseStr .= '$' . $name . '= $yunzhi_UserM->getUserById($';
+        $parseStr .= "$id"; //双引号进行变量替换
         $parseStr .= ');';
         $parseStr .= ' ?>';
         $parseStr .= $content;
