b2c7fa9e4a43d7696771bbe5bb48e91d1cde45ff
denghaoyang <275111108@qq.com>
2015-09-06 10:31:29 +0800

    提交醒目细节类

---
+++ Application/PublicProjectDetail/Model/PublicProjectDetail.class.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * 项目细节的Model类
+ *
+ * @author denghaoyang
+ * 275111108@qq.com
+ */
+namespace PublicProjectDetail\Model;
+use Think\Model;
+class PublicProjectDetailModel extends Model{
+	
+	//再取项目细节信息
+    public function getListsByIds($lists){
+    	foreach ($lists as $key => $value) {
+    		$map = array();
+    		$map['id'] = $value[pulic_project_detail_id];
+    		$value[$key][] = $this->where($map)->select();
+    	}
+    	
+
+    }
+
+    }
