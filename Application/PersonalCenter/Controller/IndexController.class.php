0355ec36d5681db6183c9e95a72af411ae589682
denghaoyang <275111108@qq.com>
2015-09-08 16:05:46 +0800

    更新个人中心动态取数据及相关方法

--- Application/PersonalCenter/Controller/IndexController.class.php
+++ Application/PersonalCenter/Controller/IndexController.class.php
@@ -3,11 +3,26 @@
 namespace PersonalCenter\Controller;
 
 use Admin\Controller\AdminController;
+use UserDepartmentPost\Model\UserDepartmentPostModel;
+use Role\Model\RoleModel;
+use User\Model\UserModel;
 
 class IndexController extends AdminController {
 
     public function indexAction() {
-        
+        $id = get_user_id();
+        //取部门岗位
+        $model = new UserDepartmentPostModel;
+        $lists = $model->getDepartmentPostInfoListsById($id);
+        //取角色
+        $RoleModel = new RoleModel;
+        //取名称
+        $role = $RoleModel->getRoleNameByUserId($id);
+        $user = new UserModel;
+        $name = $user->getUserById($id);
+        $this->assign('name',$name['name']);
+        $this->assign('role',$role);
+        $this->assign('lists',$lists);
         $this->assign('css',$this->fetch(T('indexCss')));
 //        $index = $this->fetch(T('index'));
 //        $this->show($index);
