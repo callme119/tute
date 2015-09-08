767af3dca04eeabc842c04662deaf407d125c6ed
xuao <295184686@qq.com>
2015-09-08 14:39:28 +0800

    移植page标签

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -17,13 +17,13 @@ class Page{
     public $totalRows; // 总行数
     public $totalPages; // 分页总页面数
     public $rollPage   = 11;// 分页栏每页显示的页数
-	public $lastSuffix = true; // 最后一页是否显示总页数
+    public $lastSuffix = true; // 最后一页是否显示总页数
 
     private $p       = 'p'; //分页参数名
     private $url     = ''; //当前链接URL
     private $nowPage = 1;
 
-	// 分页显示定制
+    // 分页显示定制
     private $config  = array(
         'header' => '<span class="rows">共 %TOTAL_ROW% 条记录</span>',
         'prev'   => '<<',
@@ -88,49 +88,49 @@ class Page{
 
         /* 计算分页临时变量 */
         $now_cool_page      = $this->rollPage/2;
-		$now_cool_page_ceil = ceil($now_cool_page);
-		$this->lastSuffix && $this->config['last'] = $this->totalPages;
+        $now_cool_page_ceil = ceil($now_cool_page);
+        $this->lastSuffix && $this->config['last'] = $this->totalPages;
 
         //上一页
         $up_row  = $this->nowPage - 1;
-        $up_page = $up_row > 0 ? '<a class="prev" href="' . $this->url($up_row) . '">' . $this->config['prev'] . '</a>' : '';
+        $up_page = $up_row > 0 ? '<li><a href="' . $this->url($up_row) . '"aria-label = "Previous">' . $this->config['prev'] . '</a>' : '';
 
         //下一页
         $down_row  = $this->nowPage + 1;
-        $down_page = ($down_row <= $this->totalPages) ? '<a class="next" href="' . $this->url($down_row) . '">' . $this->config['next'] . '</a>' : '';
+        $down_page = ($down_row <= $this->totalPages) ? '<li><a href="' . $this->url($down_row) . '" aria-label="Next">' . $this->config['next'] . '</a>' : '';
 
         //第一页
         $the_first = '';
         if($this->totalPages > $this->rollPage && ($this->nowPage - $now_cool_page) >= 1){
-            $the_first = '<a class="first" href="' . $this->url(1) . '">' . $this->config['first'] . '</a>';
+            $the_first = '<li><a class="first" href="' . $this->url(1) . '">' . $this->config['first'] . '</a></li>';
         }
 
         //最后一页
         $the_end = '';
         if($this->totalPages > $this->rollPage && ($this->nowPage + $now_cool_page) < $this->totalPages){
-            $the_end = '<a class="end" href="' . $this->url($this->totalPages) . '">' . $this->config['last'] . '</a>';
+            $the_end = '<a class="end" href="' . $this->url($this->totalPages) . '">' . "...".$this->config['last'] . '</a>';
         }
 
         //数字连接
         $link_page = "";
         for($i = 1; $i <= $this->rollPage; $i++){
-			if(($this->nowPage - $now_cool_page) <= 0 ){
-				$page = $i;
-			}elseif(($this->nowPage + $now_cool_page - 1) >= $this->totalPages){
-				$page = $this->totalPages - $this->rollPage + $i;
-			}else{
-				$page = $this->nowPage - $now_cool_page_ceil + $i;
-			}
+            if(($this->nowPage - $now_cool_page) <= 0 ){
+                $page = $i;
+            }elseif(($this->nowPage + $now_cool_page - 1) >= $this->totalPages){
+                $page = $this->totalPages - $this->rollPage + $i;
+            }else{
+                $page = $this->nowPage - $now_cool_page_ceil + $i;
+            }
             if($page > 0 && $page != $this->nowPage){
 
                 if($page <= $this->totalPages){
-                    $link_page .= '<a class="num" href="' . $this->url($page) . '">' . $page . '</a>';
+                    $link_page .= '<li><a class="num" href="' . $this->url($page) . '">' . $page . '</a></li>';
                 }else{
                     break;
                 }
             }else{
                 if($page > 0 && $this->totalPages != 1){
-                    $link_page .= '<span class="current">' . $page . '</span>';
+                    $link_page .= '<li><a style="background:#B6BDCF"><span class="current">' . $page . '</span></a></li>';
                 }
             }
         }
@@ -140,6 +140,6 @@ class Page{
             array('%HEADER%', '%NOW_PAGE%', '%UP_PAGE%', '%DOWN_PAGE%', '%FIRST%', '%LINK_PAGE%', '%END%', '%TOTAL_ROW%', '%TOTAL_PAGE%'),
             array($this->config['header'], $this->nowPage, $up_page, $down_page, $the_first, $link_page, $the_end, $this->totalRows, $this->totalPages),
             $this->config['theme']);
-        return "<div>{$page_str}</div>";
+        return '<div><nav><ul class="pagination">'.$page_str.'</ul></nav></div>';
     }
 }
--- ThinkPHP/Library/Think/Template/TagLib/Yunzhi.class.php
+++ ThinkPHP/Library/Think/Template/TagLib/Yunzhi.class.php
@@ -8,7 +8,10 @@ class Yunzhi extends TagLib {
 
     // 标签定义
     protected $tags   =  array(
- 		'user'       =>  array('attr'=>'id,name'),
+	'user'       =>  array('attr'=>'id,name'),
+            'page'      =>array('attr'=>'id,name,class,totalnumber','close'=>1),
+            );
+
     );
 
     public function _user($tag,$content) {
@@ -25,6 +28,22 @@ class Yunzhi extends TagLib {
         $parseStr .= ' ?>';
         $parseStr .= $content;
         
+        return $parseStr;
+    }
+    /**
+     * page标签解析
+     * 格式： <html:page id="" class="" totalnumber=""/>
+     * @access public
+     * @param array $tag 标签属性
+     * @return string|void
+     */
+    public function _page($tag){
+        $id         =   !empty($tag['id'])?$tag['id']: '_page';
+        $name       =   $tag['name'];
+        $class      =   !empty($tag['class'])?$tag['class']:'';
+        $totalnumber = !empty($tag['totalnumber'])?$tag['class']:'0';
+        $page = new Page($tag['totalnumber']);
+        $parseStr =  $page->show();
         return $parseStr;
     }
 }
