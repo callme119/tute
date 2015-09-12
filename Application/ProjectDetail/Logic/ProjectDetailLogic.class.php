7e4f3278a5d3707c3eea357793a67e0dd9052d5a
mskies <3792535@qq.com>
2015-09-12 21:11:07 +0800

    增加项目扩展信息。改变取分值的方法。

---
+++ Application/ProjectDetail/Logic/ProjectDetailLogic.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 项目扩展信息 逻辑
+ */
+namespace ProjectDetail\Logic;
+use ProjectDetail\Model\ProjectDetailModel;
+class ProjectDetailLogic extends ProjectDetailModel
+{
+	/**
+	 * 获取某项目的所有设置数值。
+	 * @param  [type] $projectId [description]
+	 * @return [type]            [description]
+	 */
+	public function getListsByProjectId($projectId)
+	{
+		$map['project_id'] = $projectId;
+		$data = $this->where($map)->select();
+
+		$return = array();
+		foreach($data as $value)
+		{
+			$return[$value['name']] = $value;
+		}
+		return $return;
+	}
+}
