f8d3dee41fc88d6819369a4506435f7452e27061
denghaoyang <275111108@qq.com>
2015-09-15 11:24:19 +0800

    实现简单的记住密码功能

--- Application/Login/Controller/IndexController.class.php
+++ Application/Login/Controller/IndexController.class.php
@@ -4,7 +4,9 @@ use Think\Controller;
 use User\Model\UserModel;
 class IndexController extends Controller {
     public function indexAction(){
-        
+        $this->assign('remember',cookie('remember'));
+        $this->assign('psw',cookie('password'));
+        $this->assign('username',cookie('username'));
         $this->assign('css',$this->fetch(T('indexCss')));
         $this->assign('js',$this->fetch(T('indexJs')));
         $login = $this->fetch(T('index'));
@@ -16,6 +18,19 @@ class IndexController extends Controller {
 
     //对用户名密码进行判断
     public function loginAction(){
+    	//检测是否勾选记住密码，传入cookie信息
+    	// if(cookie('remember') != "checked"){
+	    // 	if(I('post.remember')== 'on'){
+	    // 		cookie('password',I('post.password'));
+	    // 		cookie('username',I('post.username'));
+	    // 		cookie('remember','checked');
+	    // 	}else{
+	    // 		cookie('password',null);
+	    // 		cookie('username',null);
+	    // 		cookie('remember',null);
+	    // 	}
+    	// }
+    	echo cookie('password');
     	$model = new UserModel();
     	if($model->checkUser()){
     		$this->success('登陆成功',U('Admin/Admin/index'));
