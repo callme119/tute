0c76107e9b20e291c97ba952a36eb031a8d64b51
mskies <3792535@qq.com>
2015-09-26 11:54:48 +0800

    得写 project category WIDGET ，将数据运算放放logic

--- Application/ProjectCategoryRatio/Logic/ProjectCategoryRatioLogic.class.php
+++ Application/ProjectCategoryRatio/Logic/ProjectCategoryRatioLogic.class.php
@@ -4,6 +4,14 @@
  */
 namespace ProjectCategoryRatio\Logic;
 use ProjectCategoryRatio\Model\ProjectCategoryRatioModel;
+use Project\Model\ProjectModel; 					//项目表
+use DataModel\Model\DataModelModel;					//数据模型表
+use DataModelDetail\Model\DataModelDetailModel; 	//数据模型详情表
+use DataModelDetail\Logic\DataModelDetailLogic;		//数据模型详情
+use Project\Logic\ProjectLogic;					//项目信息表 
+use ProjectCategoryRatio\Logic\ProjectCategoryRatioLogic;	//项目类别系数表
+use ProjectCategory\Logic\ProjectCategoryLogic;		//项目类别 逻辑
+use ProjectDetail\Logic\ProjectDetailLogic;			//项目扩展信息
 class ProjectCategoryRatioLogic extends ProjectCategoryRatioModel
 {
 	/**
@@ -62,5 +70,70 @@ class ProjectCategoryRatioLogic extends ProjectCategoryRatioModel
 		$map['project_category_id'] = (int)$projectCategoryId;
 		$map['data_model_detail_id'] = (int)$dataModelDetailId;
 		return $this->where($map)->find();
+	}
+
+	/**
+	 * 通过项目ID，取出来项目的总分值
+	 * @param  [num] $id [项目ID]
+	 * @return [num]     [项目基础分值]
+	 */
+	public function getScoreByProjectId($projectId)
+	{
+		//取项目表后缀。
+		$projectId = $projectId;
+
+		$ProjectM = new ProjectModel();
+		$project = $ProjectM->where("id = $projectId")->find();
+		// dump($project);
+
+		//取项目模型信息
+		$dataModelId = $project['data_model_id'];
+		$DataModelM = new DataModelModel();
+		$dataModel = $DataModelM->getListById($dataModelId);
+		// dump($dataModel);
+
+		//取出项目模型详情中字段为select的项.
+		//同时将项目模型的所有子结点
+		$DataModelDetailL = new DataModelDetailLogic();
+		$map['html_type'] = "select";
+		$dataModelSelectRoots = 	$DataModelDetailL->getRootListsByDataModelId($dataModelId , $map);
+		$dataModelSons	=	$DataModelDetailL->getSonListsArrayByDataModelId($dataModelId);
+
+		//取项目对应的 项目分类 信息
+		$projectCategoryId = $project['project_category_id'];
+
+		$ProjectCategoryL = new ProjectCategoryLogic();
+		$projectCategory = $ProjectCategoryL->getListById($projectCategoryId);
+
+		//取项目系数信息
+		$ProjectCategoryRatioL = new ProjectCategoryRatioLogic();
+		$projectCategoryRatios = $ProjectCategoryRatioL->getListsByProjectCategoryId($projectCategoryId);
+
+		//取项目扩展信息
+		$ProjectDetailL = new ProjectDetailLogic();
+		$projectDetails = $ProjectDetailL->getListsByProjectId($projectId);
+		// dump($projectDetails);
+		// dump($projectCategory);
+
+		//取总分
+		$score = $projectCategory['score'];
+
+		//先取出name信息 ，再取出name字段对应的选项ID，再取出该ID对应的系数 。
+		foreach($dataModelSelectRoots as $root)
+		{
+			$key = $root['name'];
+
+			//如果存在，证明该系数已经设置，如果不存在，证明项目的系数未被设置。
+			//该原因，可能是先有的项目，后来增的数据模型的系数引起的。
+			//归避该BUG的方法，可以在修改数据模型前，给出是否有项目类别使用该项目模型的判断。
+			if( isset($projectDetails[$key]['value']))
+			{
+				$value = $projectDetails[$key]['value'];
+				$score =  floor($score*$projectCategoryRatios[$value]['ratio']/100);	
+			}
+		}
+
+		//输出
+		return $score;
 	}
 }
--- Application/ProjectCategoryRatio/Widget/ProjectCategoryRatioWidget.class.php
+++ Application/ProjectCategoryRatio/Widget/ProjectCategoryRatioWidget.class.php
@@ -21,60 +21,8 @@ class ProjectCategoryRatioWidget extends Controller
 	 */
 	public function getScoreByProjectIdAction($projectId)
 	{
-		//取项目表后缀。
-		$projectId = $projectId;
-
-		$ProjectM = new ProjectModel();
-		$project = $ProjectM->where("id = $projectId")->find();
-		// dump($project);
-
-		//取项目模型信息
-		$dataModelId = $project['data_model_id'];
-		$DataModelM = new DataModelModel();
-		$dataModel = $DataModelM->getListById($dataModelId);
-		// dump($dataModel);
-
-		//取出项目模型详情中字段为select的项.
-		//同时将项目模型的所有子结点
-		$DataModelDetailL = new DataModelDetailLogic();
-		$map['html_type'] = "select";
-		$dataModelSelectRoots = 	$DataModelDetailL->getRootListsByDataModelId($dataModelId , $map);
-		$dataModelSons	=	$DataModelDetailL->getSonListsArrayByDataModelId($dataModelId);
-
-		//取项目对应的 项目分类 信息
-		$projectCategoryId = $project['project_category_id'];
-
-		$ProjectCategoryL = new ProjectCategoryLogic();
-		$projectCategory = $ProjectCategoryL->getListById($projectCategoryId);
-
-		//取项目系数信息
 		$ProjectCategoryRatioL = new ProjectCategoryRatioLogic();
-		$projectCategoryRatios = $ProjectCategoryRatioL->getListsByProjectCategoryId($projectCategoryId);
-
-		//取项目扩展信息
-		$ProjectDetailL = new ProjectDetailLogic();
-		$projectDetails = $ProjectDetailL->getListsByProjectId($projectId);
-		// dump($projectDetails);
-		// dump($projectCategory);
-
-		//取总分
-		$score = $projectCategory['score'];
-
-		//先取出name信息 ，再取出name字段对应的选项ID，再取出该ID对应的系数 。
-		foreach($dataModelSelectRoots as $root)
-		{
-			$key = $root['name'];
-
-			//如果存在，证明该系数已经设置，如果不存在，证明项目的系数未被设置。
-			//该原因，可能是先有的项目，后来增的数据模型的系数引起的。
-			//归避该BUG的方法，可以在修改数据模型前，给出是否有项目类别使用该项目模型的判断。
-			if( isset($projectDetails[$key]['value']))
-			{
-				$value = $projectDetails[$key]['value'];
-				$score =  floor($score*$projectCategoryRatios[$value]['ratio']/100);	
-			}
-		}
-
+		$score = $ProjectCategoryRatioL->getScoreByProjectId($projectId);
 		//输出
 		return $score;
 	}
