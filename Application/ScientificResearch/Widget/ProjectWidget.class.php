7f9b9df6ed141707aff5b0ad93fb428f2ead56b4
myskies <3792535@qq.com>
2015-09-28 14:57:16 +0800

    重写几个widget,用继承的方法来实现

--- Application/ScientificResearch/Widget/ProjectWidget.class.php
+++ Application/ScientificResearch/Widget/ProjectWidget.class.php
@@ -3,26 +3,7 @@
  * 项目WIDGET
  */
 namespace ScientificResearch\Widget;
-use ProjectCategoryRatio\Widget\ProjectCategoryRatioWidget;
-class ProjectWidget extends ProjectCategoryRatioWidget
+use Project\Widget\ProjectWidget as ProjectW;		//项目详情
+class ProjectWidget extends ProjectW
 {
-	/**
-	 * 通过项目ID，取出来项目的系数
-	 * @param  [num] $id [项目ID]
-	 * @return [num]     [项目基础分值]
-	 */
-	public function getScoreByIdUserIdAction($id , $userId)
-	{
-		return $this->getScoreByProjectIdUserIdAction($id , $userId);
-	}
-
-	/**
-	 * 通过项目ID，取出来项目的系数
-	 * @param  [num] $id [项目ID]
-	 * @return [num]     [项目基础分值]
-	 */
-	public function getScoreByIdAction($id , $userId)
-	{
-		return $this->getScoreByProjectIdAction($id);
-	}
 }
