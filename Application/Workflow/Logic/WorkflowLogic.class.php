87c82dd4782f5fd9ccc01e562d5967ef3c026dab
myskies <3792535@qq.com>
2015-10-20 16:53:25 +0800

    增加function deleteByProjectId　function getListById

--- Application/Workflow/Logic/WorkflowLogic.class.php
+++ Application/Workflow/Logic/WorkflowLogic.class.php
@@ -6,10 +6,25 @@ namespace Workflow\Logic;
 use Workflow\Model\WorkflowModel;
 class WorkflowLogic extends WorkflowModel
 {
+
+	public function deleteByProjectId($projectId)
+	{
+		$map['project_id'] = (int)$projectId;
+		$data = $this->where($map)->delete();
+		return $data;
+	}
+	
 	public function getListByProjectId($projectId)
 	{
 		$projectId = (int)$projectId;
 		$map['project_id'] = $projectId;
 		return $this->where($map)->find();
+	}
+
+	public function getListById($id)
+	{
+		$map['id'] = (int)$id;
+		$data = $this->where($map)->find();
+		return $data;
 	}
 }
