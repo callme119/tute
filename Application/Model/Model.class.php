1d5ae327e7c4642cef7dc081a9cce5218fc8efb8
mskies <3792535@qq.com>
2016-11-23 09:35:23 +0800

    增加数据模型

---
+++ Application/Model/Model.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 模型MODEL
+ */
+namespace Model;
+
+class Model {
+    protected $data = [];
+
+    /**
+     * 获取data变量的中数据 
+     * @param    string                   $key 键值
+     * @return   array | mix | string                        
+     * @author 梦云智 http://www.mengyunzhi.com
+     * @DateTime 2016-11-23T09:34:31+0800
+     */
+    public function getData($key = '') {
+        if (empty($key)) {
+            return $this->data;
+        }
+
+        if (array_key_exists($key, $this->data)) {
+            return $this->data[$key];
+        } else {
+            return '';
+        }
+    }
+}
